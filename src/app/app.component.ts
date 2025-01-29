fafdf97e933ef25a4d1532a1266fe2efcd037d15
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2025-01-29 22:15:15 +0100

    feat: app setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<router-outlet></router-outlet>',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'zlom-fe';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth/auth.component';
+// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// import { HOME_ROUTES } from './home/home.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: AuthComponent,
+    pathMatch: 'full',
+  },
+];
