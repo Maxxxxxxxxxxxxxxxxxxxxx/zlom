57dcf296f550d595ed38453ee5ab1724dc761293
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2025-01-30 01:22:11 +0100

    fix: remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginFormComponent } from './components/login-form/login-form.component';
 import { ToastComponent } from './ui/toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginFormComponent, ToastComponent],
+  imports: [RouterOutlet, ToastComponent],
   template: ' <app-toast></app-toast><router-outlet></router-outlet>',
 })
 export class AppComponent {
