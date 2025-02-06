085498471b63611e6b0c55fb7c1d940ced9f82d3
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2025-02-06 22:46:33 +0100

    feat: add global vars to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ui-base-dark": "var(--ui-base-dark)",
+        "ui-base-dark-darker": "var(--ui-base-dark-darker)",
+        "ui-base-red": "var(--ui-base-red)",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
