02ec6779bfb385676ab694556d9e63875b4a3d32
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2025-02-06 17:21:20 +0100

    feat: connected login to backend

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { CenteredWrapperComponent } from '../../ui/centered/centered-wrapper.component';
@@ -9,6 +9,11 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+import AuthRequest from '../../dto/auth-request.dto';
+import { HotToastService } from '@ngxpert/hot-toast';
+import { Router } from '@angular/router';
+import { catchError, debounceTime, delay, of, switchMap } from 'rxjs';
 
 interface AuthData {
   username: FormControl<string | null>;
@@ -28,6 +33,12 @@ interface AuthData {
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  public constructor(
+    private authService: AuthService,
+    private toast: HotToastService,
+    private router: Router
+  ) {}
+
   loginForm: FormGroup<AuthData> = new FormGroup(
     {
       username: new FormControl('', [
@@ -43,7 +54,33 @@ export class LoginFormComponent {
   );
 
   get username() {
-    return this.loginForm.controls.username;
+    return this.loginForm.controls.username.value;
+  }
+
+  get password() {
+    return this.loginForm.controls.password.value;
+  }
+
+  login() {
+    const authRequest = {
+      username: this.loginForm.controls.username.value,
+      password: this.loginForm.controls.password.value,
+    } as AuthRequest;
+
+    this.authService
+      .login(authRequest)
+      .pipe(debounceTime(600))
+      .subscribe({
+        next: (res) => {
+          this.toast.success('Logged in!');
+          console.log(res.status);
+          this.router.navigate(['main/dashboard']);
+        },
+        error: (res) => {
+          this.toast.error(res.error.message);
+          console.log(res.status);
+        },
+      });
   }
 
   test() {
