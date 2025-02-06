0e2b3fdf3c8dd5847b23ba1f38ce1d4b4ae5b921
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2025-02-06 17:22:51 +0100

    feat: add nested route for main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth/auth.component';
+import { DashboardComponent } from './pages/main/dashboard/dashboard.component';
+import { MainComponent } from './layouts/main/main.component';
 // import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 // import { HOME_ROUTES } from './home/home.routes';
 
@@ -8,5 +10,11 @@ export const routes: Routes = [
     path: 'login',
     component: AuthComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+    pathMatch: 'full',
+    children: [{ path: 'dashboard', component: DashboardComponent }],
   },
 ];
