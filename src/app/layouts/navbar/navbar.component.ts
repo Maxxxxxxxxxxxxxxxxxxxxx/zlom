7f5fec9a75fc0aab33f1dad06861a8cb4a6dfed9
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2025-02-06 22:47:26 +0100

    feat: navbar update

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { NavLinkComponent } from '../../ui/nav-link/nav-link.component';
+import { SvgLogoComponent } from '../../ui/svg-logo/svg-logo.component';
+
+interface NavLink {
+  readonly title: string;
+  readonly path: string;
+  readonly active: boolean;
+}
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, NavLinkComponent, SvgLogoComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  navLinks: NavLink[] = [
+    { title: 'Dashboard', path: '/main/dashboard', active: true },
+    { title: 'Users', path: '/main/users', active: false },
+    { title: 'Cars', path: '/main/cars', active: false },
+    { title: 'Settings', path: '/main/settings', active: false },
+  ];
 }
