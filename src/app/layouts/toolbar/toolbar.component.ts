0e7a810d0ea9cf9cccd00dc5eb8875b0d95d6b15
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2025-02-06 22:48:44 +0100

    feat: toolbar

---
+++ src/app/layouts/toolbar/toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SearchComponent } from '../../ui/search/search.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [SearchComponent, FormsModule],
+  templateUrl: './toolbar.component.html',
+})
+export class ToolbarComponent {
+
+  
+  public searchPlaceholder: string = 'Search all...';
+}
