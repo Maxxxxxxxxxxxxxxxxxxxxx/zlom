53e41a97584b4d6624f7bd9e7e8b99d2349636f2
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2025-02-08 07:40:13 +0100

    feat: navbar update

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { NavLinkComponent } from '../../ui/nav-link/nav-link.component';
 import { SvgLogoComponent } from '../../ui/svg-logo/svg-logo.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HotToastService } from '@ngxpert/hot-toast';
+import { Router } from '@angular/router';
 
 interface NavLink {
   readonly title: string;
@@ -13,14 +16,28 @@ interface NavLink {
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, NavLinkComponent, SvgLogoComponent],
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    NavLinkComponent,
+    SvgLogoComponent,
+    MatIconModule,
+  ],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  private toast: HotToastService = inject(HotToastService);
+  private router: Router = inject(Router);
+
   navLinks: NavLink[] = [
     { title: 'Dashboard', path: '/main/dashboard', active: true },
-    { title: 'Users', path: '/main/users', active: false },
+    { title: 'User management', path: '/main/users', active: false },
     { title: 'Cars', path: '/main/cars', active: false },
     { title: 'Settings', path: '/main/settings', active: false },
   ];
+
+  logout() {
+    this.toast.success('Logged out!');
+    this.router.navigate(['login']);
+  }
 }
